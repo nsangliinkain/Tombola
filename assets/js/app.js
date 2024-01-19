f49629b20a676b9f6fc45ca664b1ed3bf5dcf535
Dfolli <danilo.folli@itis.pr.it>
2024-01-19 09:40:28 +0100

    struttuazione codice

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+let righe = 10;
+let colonne = 10;
+
+function DisegnaTabella() {
+
+    let div1 = document.getElementById('div1');
+    let tabella = document.createElement("table");
+
+    var i = 1;
+    for (var r = 0; r < righe; r++) {
+        let row = document.createElement("tr");
+        for (var c = 0; c < colonne; c++, i++) {
+            let cell = document.createElement("td");
+            let cellText = document.createTextNode(i);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        tabella.appendChild(row);
+    }
+
+    div1.appendChild(tabella);
+
+}
+
+DisegnaTabella();
