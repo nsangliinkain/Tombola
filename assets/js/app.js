b2a9be696c6afc2e4a4370016bd4fb9267df15e1
Nsanglii Nkain <nsangliidzela04@gmail.com>
2024-01-19 17:07:31 +0100

    final work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,12 @@
 let righe = 10;
 let colonne = 10;
+let numeriEstratti = [];
 
 function DisegnaTabella() {
-
     let div1 = document.getElementById('div1');
+    while (div1.firstChild) {
+        div1.removeChild(div1.firstChild);
+    }
     let tabella = document.createElement("table");
 
     var i = 1;
@@ -14,6 +17,7 @@ function DisegnaTabella() {
             let cellText = document.createTextNode(i);
             cell.appendChild(cellText);
             row.appendChild(cell);
+            if (numeriEstratti.includes(i)) cell.style.backgroundColor = 'red';
         }
         tabella.appendChild(row);
     }
@@ -23,26 +27,6 @@ function DisegnaTabella() {
 
 function estraiNumero(){
     var num =  Math.floor(Math.random() * 100 + 1);
-    alert(num);
-    let div1 = document.getElementById('div1');
-    let tabella = document.createElement("table");
-
-    var i = 1;
-    for (var r = 0; r < righe; r++) {
-        let row = document.createElement("tr");
-        for (var c = 0; c < colonne; c++, i++) {
-            let cell = document.createElement("td");
-            let cellText = document.createTextNode(i);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-            if (cellText == num) cell.style.backgroundColor = 'red';
-        }
-        tabella.appendChild(row);
-    }
-
-    div1.
+    numeriEstratti.push(num);
+    DisegnaTabella();
 }
-
-DisegnaTabella();
-
-
